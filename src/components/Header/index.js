079c1ada0e1b5a091a8890e5dee79828e4c2b5c2
starunz <busouza01@gmail.com>
2022-02-14 09:45:11 -0500

    cart in sidebar implemented

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,8 @@
 import { Container, ShinobiStore } from "./style";
 
-import { IoCartOutline } from 'react-icons/io5'
-
 import Sidebar from "./sideBar/SideBar";
-import { useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2';
-import kakashi from '../../assets/images/kakashin.png';
-import useAuth from "../../hooks/useAuth";
 
 export default function Header() {
-
-    const { auth } = useAuth();
-    const navigate = useNavigate();
-
-    function handleCart() {
-        if (auth) {
-            navigate('/cart')
-        } else {
-
-            Swal.fire({
-                title: 'OOPS...',
-                text: "Você precisa estar logado para visualizar o carrinho.",
-                imageUrl: `${kakashi}`,
-                imageHeight: 140,
-                showCancelButton: true,
-                confirmButtonColor: '#1C1C1C',
-                cancelButtonColor: '#b8b8b8',
-                confirmButtonText: 'Logar',
-                cancelButtonText: 'Cancelar'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    navigate('/login');
-                }
-            })
-        }
-    }
 
     return (
         <Container>
@@ -42,13 +10,6 @@ export default function Header() {
 
             <ShinobiStore>Shinobi Tool Store</ShinobiStore>
 
-            <IoCartOutline onClick={handleCart}
-                color={'#ffffff'}
-                title={'cart'}
-                height="30px"
-                width="30px"
-                fontSize="35px"
-            />
         </Container>
     );
 }
